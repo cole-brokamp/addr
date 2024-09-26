3ec0d41726fc07be785450ad69ed1f164ef6c012
Cole Brokamp <cole.brokamp@gmail.com>
2024-09-25 22:21:49 -0400

    better geocoding comparison tables

--- inst/compare_geocoding_to_degauss.R
+++ inst/compare_geocoding_to_degauss.R
@@ -40,10 +40,46 @@ degauss_s2 <-
 
 # doing this instead of a left_join because when some addr objects with missing address numbers
 # are converted to character, they are no longer unique
-d <- dplyr::bind_cols(arrange(addr_s2, addr), degauss_s2 = arrange(degauss_s2, addr)$degauss_s2)
-
-d |>
+d <-
+  dplyr::bind_cols(arrange(addr_s2, addr), degauss_s2 = arrange(degauss_s2, addr)$degauss_s2) |>
   mutate(
     addr_bg = tiger_block_groups(addr_s2, year = "2020"),
-    degauss_bg = tiger_block_groups(degauss_s2, year = "2020")
+    degauss_bg = tiger_block_groups(degauss_s2, year = "2020"),
+    s2_dist = s2_cell_distance(addr_s2, degauss_s2)
   )
+
+# matching results
+d |>
+  mutate(
+    degauss_matched = !is.na(degauss_s2),
+    addr_matched = !is.na(addr_s2)
+  ) |>
+  summarize(
+    n = n(),
+    .by = c(addr_matched, degauss_matched)
+  ) |>
+  mutate(perc = scales::percent(n / sum(n), accuracy = 0.1)) |>
+  arrange(desc(n)) |>
+  knitr::kable()
+
+# agreement on census geographies among both matched addresses
+d |>
+  filter(!is.na(degauss_s2) & !is.na(addr_s2)) |>
+  mutate(
+    bg_agree = addr_bg == degauss_bg,
+    ct_agree = substr(addr_bg, 1, 11) == substr(degauss_bg, 1, 11)
+  ) |>
+  summarize(
+    n = n(),
+    across(
+      c(s2_dist),
+      list(
+        "ptiles (5th, 25th, 50th, 75th, 95th)" =
+          \(.) paste(round(quantile(., probs = c(0.05, 0.25, 0.5, 0.75, 0.95)), 1), collapse = ", ")
+      )
+    ),
+    .by = c(ct_agree, bg_agree)
+  ) |>
+  mutate(perc = scales::percent(n / sum(n), accuracy = 0.1)) |>
+  arrange(desc(n)) |>
+  knitr::kable(digits = 1)
